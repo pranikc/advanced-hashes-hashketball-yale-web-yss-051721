a1e52f9a79ad2cfd0144294817318e3d3c714730
abstract-instructions-1268 <pranik.chainani@yale.edu>
2021-06-06 21:40:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -162,29 +162,29 @@ def team_names
   end
 end
 
-def player_numbers(input)
-  output = []
-  game_hash.each do |team, team_info|
-    if team_info[:team_name] == input 
-      team_info.each do |key, value|
+def player_numbers(team_name)
+  results = []
+  game_hash.each do |location, team_data|
+    if team_data[:team_name] == team_name 
+      team_data.each do |key, value|
         if key == :players
           value.each do |player|
-          output.push(player[:number])
+          results.push(player[:number])
           end
         end
       end
     end
   end
-  return output
+  return results
 end
 
-def player_stats(input)
-  game_hash.each do |team, team_info|
-    team_info.each do |key, value|
+def player_stats(player_name)
+  game_hash.each do |location, team_data|
+    team_data.each do |key, value|
       if key == :players
         value.each do |player|
-          if input == player[:player_name]
-            player.delete(:player_name) # having player name inside the hash was a bad idea!
+          if player_name == player[:player_name]
+            player.delete(:player_name)
             return player
           end
         end
@@ -196,8 +196,8 @@ end
 def big_shoe_rebounds
   big_shoe = 0
   rebounds = 0
-  game_hash.each do |team, team_info|
-    team_info[:players].each do |player|
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
       if player[:shoe] > big_shoe
         big_shoe = player[:shoe]
         rebounds = player[:rebounds]
