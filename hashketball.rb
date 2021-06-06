f961ca7376540bcfa5787feb7e77e18e04d381ff
abstract-instructions-1268 <pranik.chainani@yale.edu>
2021-06-06 21:42:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -184,7 +184,6 @@ def player_stats(player_name)
       if key == :players
         value.each do |player|
           if player_name == player[:player_name]
-            player.delete(:player_name)
             return player
           end
         end
